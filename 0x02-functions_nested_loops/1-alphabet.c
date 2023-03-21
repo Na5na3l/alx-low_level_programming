e960ec7584815849caaa65fc67683b8a1f62be8a
na5na3l <natnaelarega9@gmail.com>
2023-03-21 11:31:23 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,20 @@
-#include"main.h"
+#include "main.h"
 
-/**
- * main - print small letter alphabets
- * print_alphabet - print all alphabet in lowercase 
- * Return: always (0)
- */
-void print_alphabet(void)
-{
-	char letter;
 
-	for(letter = 'a';letter <= 'z'; letter++)
+	/**
+	 * print_alphabet - print all alphabet in lowercase
+	 */
+
+
+	void print_alphabet(void)
 	{
-		_putchar(letter);
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
 	}
-	_putchar('\n');
-}
+
