21823ac578ad66261dbff8853be316a4cfbbaf03
na5na3l <natnaelarega9@gmail.com>
2023-03-23 23:57:03 +0300

    error correction

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
  */
 int main(void)
 {
-	int i;
+int i;
 
-	printf("infinite loop incoming ;(\n");
+printf("infinite loop incoming ;(\n");
 
-	i=0;
+i=0;
 
-	/*while (i < 10)*/
-	  /*{*/
-	  /*putchar(i);*/
-	  /*}*/
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
 
-	printf("infinite loop avoided \\0/\n");
+printf("infinite loop avoided \\0/\n");
 
-	return(0);
+return(0);
 }
